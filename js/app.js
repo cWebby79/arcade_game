e368291e5e348b09685480539a541a1cbe52aeae
Chris Webster <chris.webster79@gmail.com>
2018-07-04 17:23:27 +0100

    Add player render

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
+//****************************** ENEMY ****************************** //
+
 // Enemies our player must avoid
-const Enemy = function(x,y) {
+const Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -8,6 +10,7 @@ const Enemy = function(x,y) {
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -23,16 +26,23 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+//****************************** PLAYER ***************************** //
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+// Player
 const Player = function(x,y) {
     this.sprite = 'images/char-horn-girl.png';
     this.x = x;
     this.y = y;
 };
 
+// Draw Player on screen
+Player.prototype.render = function() {
+    ctx.drawing(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
